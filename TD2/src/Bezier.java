3d77585bb0b987f6dd045f5d36f5c4ab4e1b6827
uvsq21436959 <colin.simon78@gmail.com>
2017-10-09 11:23:14 +0200

    Bernstein polynomials works

--- TD2/src/Bezier.java
+++ TD2/src/Bezier.java
@@ -23,7 +23,7 @@ public class Bezier extends Curve {
 	 * Draw the control polygon
 	 */
 	public void drawControlPolygon() {
-		this.frame.stroke(0, 0, 255);
+	//	this.frame.stroke(0, 0, 255);
 	    for(int i=1;i<this.points.length;i++) {
 	    	drawSegment(this.points[i], this.points[i-1]);
 	    	//drawSegment(transformation.transform(points[i]), transformation.transform(points[i-1]));
@@ -37,8 +37,8 @@ public class Bezier extends Curve {
 	 */
 	public Point_2 evaluate(double t) {
 		//return recursiveDeCasteljau(this.points.length-1, 0, t);
-		return iterativeDeCasteljau(t);
-		//return bernsteinBezier(t);
+		//return iterativeDeCasteljau(t);
+		return bernsteinBezier(t);
 	}
 
 	/**
@@ -69,23 +69,6 @@ public class Bezier extends Curve {
 			b0[i + 1] = array[0];
 			b1[i + 1] = array[n - i - 1];
 		}
-/*		Point_2[] array = this.points.clone();
-		for(int i = 0; i < array.length - 1; i++)
-		{
-			for(int j = 0; j < array.length - 1 - i; j++)
-			{
-				Point_2 a = array[j + 1].multiplyByScalar(t);
-				Point_2 b = array[j].multiplyByScalar(1 - t);
-				Vector_2 v = new Vector_2(b.x, b.y);
-				array[j] = a.sum(v);
-			}
-			b0[i] = array[0];
-			b1[i] = array[n - i];
-		} */
-/* 		Point_2 a = b0[n - 1].multiplyByScalar(t);
-		Point_2 b = b1[n - 1].multiplyByScalar(1 - t);
-		b0[n] = a.sum(new Vector_2(b.x, b.y));
-		b1[n] = b0[n]; */
 		result[0] = new Bezier(this.frame, b0, this.transformation);
 		result[1] = new Bezier(this.frame, b1, this.transformation);
 		
@@ -98,7 +81,7 @@ public class Bezier extends Curve {
 	 * Plot the curve (in the frame), for t=0..1, with step dt
 	 */
 	public void plotCurve(double dt) {
-	//	this.frame.stroke(255, 255, 255);
+		this.frame.stroke(0, 0, 255);
 		this.drawControlPolygon();
 		this.drawControlPoints();
 		Point_2 a = evaluate(0);
@@ -108,12 +91,14 @@ public class Bezier extends Curve {
 			b = evaluate(i);
 			drawSegment(a, b);
 			a = b;
-		}
+		} 
 		Bezier[] sub = subdivide(0.5);
-		sub[0].frame.stroke(255, 0, 0);
-		sub[0].drawControlPolygon();
-		sub[1].drawControlPolygon();
+		this.frame.stroke(255, 0, 0);
+	//	sub[0].drawControlPolygon();
+		this.frame.stroke(255, 0, 0);
+	//	sub[1].drawControlPolygon();
 		
+		//subdivisionRendering(10);
 		// to be completed TD2
 	}
 
@@ -125,7 +110,22 @@ public class Bezier extends Curve {
 		this.drawControlPolygon(); // draw original control polygon
 		this.drawControlPoints(); // draw original control points
 		LinkedList<Bezier> subCurves=new LinkedList<Bezier>();
-		
+		subCurves.add(this);
+		for(int i = 0; i < n; i++)
+		{
+			LinkedList<Bezier> tmp = new LinkedList<Bezier>();
+			for(Bezier b : subCurves)
+			{
+				Bezier[] res = b.subdivide(0.5);
+				tmp.add(res[0]);
+				tmp.add(res[1]);
+			}
+			subCurves = tmp;
+		}
+		for(Bezier b : subCurves)
+		{
+			b.drawControlPolygon();
+		}
 		// to be completed TD2
 		if(this.points.length<3) return;
 
@@ -165,22 +165,13 @@ public class Bezier extends Curve {
 	
 	public int binco(int n, int i)
 	{
-		if(i == 0 || i == n)
-				return n;
+		if(i < 0 || i > n)
+			return 0;
+		else if(i == 0 || i == n)
+			return 1;
 		else
 			return (n - i + 1) * binco(n, i - 1) / i;
 	}
-	
-/*	public Point_2 bernsteinBezier(double dt)
-	{
-		int n = this.points.length;
-		int[] bincos = new int[n];
-		bincos[0] = 1;
-		for(int i = 1; i < n; i++)
-			bincos[i] = bincos[i - 1] * (n - i + 1) / i;
-		for(double i = dt; i < 1; i += dt)
-			
-	} */
 	
 	public Point_2 bernsteinBezier(double dt)
 	{
@@ -189,14 +180,16 @@ public class Bezier extends Curve {
 		double t = dt;
 		Point_2 pt;
 		Vector_2 v;
-		Point_2 out = this.points[0].multiplyByScalar(binco(n, 0) * s);
+		Point_2 out = this.points[0].multiplyByScalar(binco(n - 1, 0));
 		for(int i = 1; i < n; i++)
 		{
-			pt = this.points[i].multiplyByScalar(binco(n, i) * t);
+			pt = this.points[i].multiplyByScalar(binco(n - 1, i) * t);
 			v = new Vector_2(pt.x, pt.y);
-			out = out.multiplyByScalar(s).sum(v);
+			out = out.multiplyByScalar(s);
+			out = out.sum(v);
 			t = t * dt;
 		}
+		System.out.println("here");
 		return out;
 	}
 
